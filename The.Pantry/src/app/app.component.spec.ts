f51e7ad428ffd00e278ca6ec622b83323a9f9219
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2018-08-15 16:16:15 +0200

    setup and first stuff

--- The.Pantry/src/app/app.component.spec.ts
+++ The.Pantry/src/app/app.component.spec.ts
@@ -1,27 +1,55 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'the-pantry'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('the-pantry');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the-pantry!');
-  }));
+    //let data;
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    let myElementCollection: HTMLElement[];
+
+    beforeEach(async(() => {
+        //data = [];
+
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.debugElement.componentInstance;
+    }));
+
+    it('should create the app', async(() => {
+        expect(component).toBeTruthy();
+    }));
+
+    it("data should be empty", () => {
+        expect(component.data.length).toBe(0);
+    });
+
+    it("data should no longer be empty", () => {
+        component.data = ["item1", "item2"];
+        fixture.detectChanges();
+        expect(component.data.length).toBe(2);
+    });
+
+    it("li should have content", () => {
+        component.data = ["item1", "item2"];
+        fixture.detectChanges();
+        myElementCollection = fixture.nativeElement.querySelectorAll("li");
+        expect(myElementCollection.length).toBe(2, "nothing displayed");
+    });
+
+    //it(`should have as title 'the-pantry'`, async(() => {
+    //    const fixture = TestBed.createComponent(AppComponent);
+    //    const app = fixture.debugElement.componentInstance;
+    //    expect(app.title).toEqual('the-pantry');
+    //}));
+
+    //it('should render title in a h1 tag', async(() => {
+    //    const fixture = TestBed.createComponent(AppComponent);
+    //    fixture.detectChanges();
+    //    const compiled = fixture.debugElement.nativeElement;
+    //    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the-pantry!');
+    //}));
 });
--- The.Pantry/src/app/app.component.ts
+++ The.Pantry/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+//import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'the-pantry';
-  public values: string[];
+    //title = 'the-pantry';
+    //public values: string[];
 
-  constructor(private http: Http) {
-    this.http.get('/api/values').subscribe(result => {
-      this.values = result.json() as string[];
-    }, error => console.error(error));
-  }
+    //constructor(private http: Http) {
+    //    this.http.get('/api/values').subscribe(result => {
+    //        this.values = result.json() as string[];
+    //    }, error => console.error(error));
+    //}
+
+    data = [];
+
+    constructor() {}
 }
